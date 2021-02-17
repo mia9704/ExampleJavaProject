32fd48751cd62afbc117765612756816b4024768
mia <mia@192.168.1.11>
2021-02-17 22:50:38 +0800

    Changed add method and added subtract method

--- ExampleJavaProject/src/main/java/Main.java
+++ ExampleJavaProject/src/main/java/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
-	public int add(int num1, int num2) {
-		return num1 + num2;
+	public int add(int num1, int num2, int num3) {
+		return num1 + num2 + num3;
+	}
+	
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
 	}
 }
